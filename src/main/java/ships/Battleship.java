d40bab9e5210f6fe5763473c6273ee553e7f5aa4
Idde-boop <145914744+Idde-boop@users.noreply.github.com>
2023-10-31 16:42:52 +0100

    Created Ships and Fleet classes

---
+++ src/main/java/ships/Battleship.java
@@ -0,0 +1,45 @@
+package ships;
+
+public class Battleship extends Ship{
+    public Battleship() {
+    }
+
+    public Battleship(String name, int size, boolean isDestroyed) {
+        super(name, size, isDestroyed);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public void setSize(int size) {
+        super.setSize(size);
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return super.isDestroyed();
+    }
+
+    @Override
+    public void setDestroyed(boolean destroyed) {
+        super.setDestroyed(destroyed);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/ships/Fleet.java
@@ -0,0 +1,61 @@
+package ships;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fleet {
+
+    private List<Ship> playerFleet;
+
+    public Fleet() {
+        this.playerFleet = new ArrayList<Ship>();
+
+        AircraftCarrier aircraftCarrier = new AircraftCarrier("AircraftCarrier", 5,false);
+
+        Battleship battleship1 = new Battleship("Battleship1", 4, false );
+        Battleship battleship2 = new Battleship("Battleship2", 4, false);
+
+        Cruiser cruiser1 = new Cruiser("Cruiser1", 3, false);
+        Cruiser cruiser2 = new Cruiser("Cruiser2", 3, false);
+        Cruiser cruiser3 = new Cruiser("Cruiser3", 3, false);
+
+        Submarine submarine1 = new Submarine("Submarine1", 2, false);
+        Submarine submarine2 = new Submarine("Submarine2", 2, false);
+        Submarine submarine3 = new Submarine("Submarine3", 2, false);
+        Submarine submarine4 = new Submarine("Submarine4", 2, false);
+
+       this.playerFleet.add(aircraftCarrier);
+
+       this.playerFleet.add(battleship1);
+       this.playerFleet.add(battleship2);
+
+       this.playerFleet.add(cruiser1);
+       this.playerFleet.add(cruiser2);
+       this.playerFleet.add(cruiser3);
+
+       this.playerFleet.add(submarine1);
+       this.playerFleet.add(submarine2);
+       this.playerFleet.add(submarine3);
+       this.playerFleet.add(submarine4);
+
+    }
+
+    public Fleet(List<Ship> playerFleet) {
+        this.playerFleet = playerFleet;
+    }
+
+    public List<Ship> getPlayerFleet() {
+        return playerFleet;
+    }
+
+    public void setPlayerFleet(List<Ship> playerFleet) {
+        this.playerFleet = playerFleet;
+    }
+
+    @Override
+    public String toString() {
+        return "Fleet{" +
+                "playerFleet=" + playerFleet +
+                '}';
+    }
+}

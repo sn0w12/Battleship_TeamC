10267d0457b962283327adbb46b76c89e9b2b669
sn0w12 <lucasskold49@gmail.com>
2023-10-31 13:10:06 +0100

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.battleship_teamc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.battleship_teamc to javafx.fxml;
+    exports com.example.battleship_teamc;
+}

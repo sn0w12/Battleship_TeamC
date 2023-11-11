eb05f851e754ea9e9d5a80d88f4030f02b91ba3f
sn0w12 <lucasskold49@gmail.com>
2023-11-11 02:17:14 +0100

    Small cleanup

--- src/main/java/com/example/battleship_teamc/Logic.java
+++ src/main/java/com/example/battleship_teamc/Logic.java
@@ -1,4 +1,3 @@
-
 package com.example.battleship_teamc;
 
 import ships.*;
--- src/main/java/com/example/battleship_teamc/Server.java
+++ src/main/java/com/example/battleship_teamc/Server.java
@@ -6,7 +6,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
 public class Server {
     public static void main(String[] args) {
         try {

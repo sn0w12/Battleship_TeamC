65cf933304456936f61f2cb74cff065bdfa76842
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2023-11-14 15:00:26 +0100

    Tagit bort ServerController och ändrat ClientController till Gamecontroller som kommer styra grafiska uppdateringen oavsett om man är klient eller server

--- src/main/java/com/example/battleship_teamc/GameController.java
+++ src/main/java/com/example/battleship_teamc/GameController.java
@@ -1,5 +1,5 @@
 package com.example.battleship_teamc;
 
-public class ClientController {
+public class GameController {
 }
 //Logik och metoder för hur Klienten fungerar, inkl uppstart från klientsidan och uppdatering av spelbrädan när den körs.
--- src/main/java/com/example/battleship_teamc/ServerController.java
+++
@@ -1,5 +0,0 @@
-package com.example.battleship_teamc;
-
-public class ServerController {
-    //Logik och metoder för hur Servern fungerar, inkl uppstart från serversidan och uppdatering av spelbrädan när den körs.
-}

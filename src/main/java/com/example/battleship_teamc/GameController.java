f34de52980da07344bf67975ab7d1fcd6f4125e6
sn0w12 <lucasskold49@gmail.com>
2023-11-16 11:51:51 +0100

    Merged with Main

--- src/main/java/com/example/battleship_teamc/GameController.java
+++ src/main/java/com/example/battleship_teamc/GameController.java
@@ -12,27 +12,53 @@ import java.util.Random;
 public class GameController {
 
     @FXML
-    private GridPane ClientGrid;
-    private final Board gameBoard;
-    private final Logic gameLogic;
-    private static final int GRID_SIZE = 10;
-    private final Fleet playerFleet;
+    private GridPane clientGrid;
 
-    public GameController() {
-        this.gameBoard = new Board(GRID_SIZE, GRID_SIZE);
-        this.playerFleet = new Fleet();
-        this.gameLogic = new Logic(gameBoard, playerFleet);
+    @FXML
+    private GridPane serverGrid;
+    private GridPane tempGrid;
+    private boolean isServer;
+    private final Board clientBoard;
+    private final Board serverBoard;
+    private final Logic clientLogic;
+    private final Logic serverLogic;
+    private static final int GRID_SIZE = 10;
+    private final Fleet clientFleet;
+    private final Fleet serverFleet;
+
+    public boolean isServer() {
+        return isServer;
     }
 
-    public void placeShipsRandomly(Board gameBoard, Fleet playerFleet) {
+    public void setServer(boolean server) {
+        isServer = server;
+    }
+
+    public GridPane getClientGrid() {
+        return clientGrid;
+    }
+    public GridPane getServerGrid() {
+        return serverGrid;
+    }
+
+    public GameController() {
+        this.clientBoard = new Board(GRID_SIZE, GRID_SIZE);
+        this.serverBoard = new Board(GRID_SIZE, GRID_SIZE);
+        this.clientFleet = new Fleet();
+        this.serverFleet = new Fleet();
+        this.clientLogic = new Logic(clientBoard, clientFleet);
+        this.serverLogic = new Logic(serverBoard, serverFleet);
+    }
+
+    public void placeShipsRandomly(GridPane grid, Board gameBoard, Fleet playerFleet, Logic gameLogic) {
         gameBoard.clearBoard();
         playerFleet.resetFleet();
         gameLogic.placeShips(gameBoard, playerFleet);
-        updateGridPaneFromBoard();
+        updateGridPaneFromBoard(grid, gameBoard);
     }
 
-    private void updateGridPaneFromBoard() {
-        ClientGrid.getChildren().clear();
+    private void updateGridPaneFromBoard(GridPane grid, Board gameBoard) {
+        grid.getChildren().clear();
         for (int row = 0; row < GRID_SIZE; row++) {
             for (int col = 0; col < GRID_SIZE; col++) {
                 Rectangle rectangle = new Rectangle(20, 20);
@@ -45,15 +71,19 @@ public class GameController {
                     default -> rectangle.setFill(Color.BLUE); // Water
                 }
 
-                ClientGrid.add(rectangle, col, row);
+                grid.add(rectangle, col, row);
             }
         }
     }
 
-    public void initialize() {
-        // This method is called after the FXML file has been loaded.
-        ClientGrid.getChildren().remove(1, ClientGrid.getChildren().size());
+    public void placeShipsOnMap() {
         Fleet fleet = new Fleet();
-        placeShipsRandomly(gameBoard, playerFleet);
+        if (isServer){
+            serverGrid.getChildren().remove(1, serverGrid.getChildren().size());
+            placeShipsRandomly(serverGrid, serverBoard, fleet, serverLogic);
+        } else {
+            clientGrid.getChildren().remove(1, clientGrid.getChildren().size());
+            placeShipsRandomly(clientGrid, clientBoard, fleet, clientLogic);
+        }
     }
 }

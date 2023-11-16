97a13784ee400d12cbf7c67124adb03c98522801
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2023-11-16 10:34:23 +0100

    Placering av skepp funkar på båda kartorna beroende på vilken knapp man trycker på i första fönstret

--- src/main/java/com/example/battleship_teamc/HelloApplication.java
+++ src/main/java/com/example/battleship_teamc/HelloApplication.java
@@ -1,14 +1,23 @@
 package com.example.battleship_teamc;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    @FXML
+    private GridPane clientGrid;
+    @FXML
+    private GridPane serverGrid;
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -17,24 +26,32 @@ public class HelloApplication extends Application {
         stage.setScene(firstScene);
         stage.show();
 
-        Button startClient = (Button) firstScene.lookup("#playAsClient");
-        Button startServer = (Button) firstScene.lookup("#playAsServer");
+        Button playAsClient = (Button) firstScene.lookup("#playAsClient");
+        Button playAsServer = (Button) firstScene.lookup("#playAsServer");
 
-        startClient.setOnAction(event -> {
+        playAsClient.setOnAction(event -> {
             try {
                 FXMLLoader clientLoader = new FXMLLoader(HelloApplication.class.getResource("GameView.fxml"));
                 Scene clientScene = new Scene(clientLoader.load());
                 stage.setScene(clientScene);
+
+                GameController controller = clientLoader.getController();
+                controller.setServer(false);
+                controller.placeShipsOnMap();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
-        startServer.setOnAction(event -> {
+        playAsServer.setOnAction(event -> {
             try {
                 FXMLLoader serverLoader = new FXMLLoader(HelloApplication.class.getResource("GameView.fxml"));
                 Scene serverScene = new Scene(serverLoader.load());
                 stage.setScene(serverScene);
+
+                GameController controller = serverLoader.getController();
+                controller.setServer(true);
+                controller.placeShipsOnMap();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -43,4 +60,5 @@ public class HelloApplication extends Application {
     public static void main (String[]args){
         launch();
     }
+
 }
